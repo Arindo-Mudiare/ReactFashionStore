0c256d43212883c9c02fd850fc60e1359fe2a9ce
Arindo-Mudiare <arindoTmudiare@gmail.com>
2019-09-02 15:18:49 +0100

    created basic routing with next js and wired up a basic next store for state of the application

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Home = props => (
+  <div>
+    <p>Home Page Alhaji</p>
+    <Link href="/sell">
+      <a>Sell Nwanne</a>
+    </Link>
+  </div>
+);
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Sell = props => (
+  <div>
+    <p>Sell Page Nwanne</p>
+    <Link href="/">
+      <a>Back 2 Home</a>
+    </Link>
+  </div>
+);
+
+export default Sell;
